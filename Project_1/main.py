01dc6efaae4536ea55570a02b8e7e0ff06d8e7cf
K-Krut <katiakrut448@gmail.com>
2023-02-26 22:11:38 +0200

    neuron not

--- Project_1/main.py
+++ Project_1/main.py
@@ -9,7 +9,10 @@ class Neuron:
         return self.weights
 
     def set_weights(self, w):
-        self.weights = w
+        if isinstance(w, list):
+            self.weights = w
+        if w is None:
+            self.weights = [1, 1]
 
     def get_S(self, inputs):
         return np.dot(np.array(self.weights), inputs)
@@ -29,55 +32,75 @@ class NeuronAND(Neuron):
             self.weights = [1, 1]
         super().__init__(weights)
         self.weights = weights
-
-    def set_weights(self, w):
-        if isinstance(w, list):
-            self.weights = w
-        if w is None:
-            self.weights = [1, 1]
+        self.name = 'Neuron AND'
 
     def act_F(self, weighted):
         return 1 if weighted >= 1.5 else 0
 
-    def forward(self, inputs):
-        # print(self.weights, inputs)   print(np.dot(self.weights, inputs))
+    def forward(self, inputs):  # print(self.weights, inputs)   print(np.dot(self.weights, inputs))
         return self.act_F(self.get_S(inputs))
 
+    def __str__(self):
+        return f'{self.name}\nweights: {self.weights}\n'
+
+
+class NeuronNOT(Neuron):
+    def __init__(self, weight):
+        if isinstance(weight, float):
+            self.weights = weight
+        if weight is None:
+            self.weights = -1.5
+        super().__init__(weight)
+        self.weights = weight
+        self.name = 'Neuron NOT'
+
+    def set_weights(self, w):
+        if isinstance(w, float):
+            self.weights = w
+        if w is None:
+            self.weights = 1
+
+    def get_S(self, inputs):
+        return self.weights * inputs
+
+    def act_F(self, weighted):
+        return 1 if weighted >= -1 else 0
+
+    def forward(self, inputs):
+        return self.act_F(self.get_S(inputs))
+
+    def __str__(self):
+        return f'{self.name}\nweights: {self.weights}\n'
 
 
 class NeuralNetworkLO:
-    def __init__(self, neuron, data, weights=None):
+    def __init__(self, neuron, data):
         self.neuron = neuron  # self.neuron = NeuronAND([1, 1])
-        self.neuron.set_weights(weights)
         self.data = data
+        self.result = []
 
     def forward(self, inputs):
         return self.neuron.forward(inputs)
-    
+
     def check_data(self):
-        for i in self.data:
-            print(self.forward(i))
+        return [(i, self.forward(i)) for i in self.data]
 
-
-neuron_types = {
-    'AND': NeuronAND(None)
-}
+    def __str__(self):
+        if not self.result:
+            self.result = self.check_data()
+        return f'{str(self.neuron)}\n' + '\n'.join([f'{i[0]} => {i[1]}' for i in self.result]) + '\n---------------\n'
 
 
 data_AND = [
     (0, 0),  # Expected output: 0
     (0, 1),  # Expected output: 0
     (1, 0),  # Expected output: 0
-    (1, 1)   # Expected output: 1
+    (1, 1)  # Expected output: 1
+]
+data_NOT = [
+    0,  # Expected output: 1
+    1,  # Expected output: 0
 ]
 
-nn = NeuralNetworkLO(NeuronAND([1, 1]), data_AND)
-
-
-
-
-# Test the neural network with inputs (0, 0), (0, 1), (1, 0), and (1, 1)
-print(nn.forward(np.array([0, 0])))  # Expected output: 0
-print(nn.forward(np.array([0, 1])))  # Expected output: 0
-print(nn.forward(np.array([1, 0])))  # Expected output: 0
-print(nn.forward(np.array([1, 1])))  # Expected output: 1
+print(NeuralNetworkLO(NeuronAND([1, 1]), data_AND))
+print(NeuralNetworkLO(NeuronNOT(-1.5), data_NOT))
