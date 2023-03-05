83a0ffeb12f94b70c29e8161ae4aa62fc0db6cc4
K-Krut <katiakrut448@gmail.com>
2023-03-05 23:54:38 +0200

    series test

--- Project_1/series.py
+++ Project_1/series.py
@@ -57,20 +57,6 @@ class Neuron:
         return f'{self.name}\nweights: {self.weights}\nf(S): {self.neuron_fS}\ndelta: {self.delta}'
 
 
-"""
-    def get_input_size(self):
-        return self.input_size
-
-    def get_hidden_size(self):
-        return self.hidden_size
-
-    def get_output_size(self):
-        return self.output_size
-
-    def get_iterations(self):
-        return self.iterations"""
-
-
 class NeuralNetwork:
     def __init__(self, weights_=None, iterations=1000, lmd=0.1, input_size=3, hidden_size=4, output_size=1):
         self.input_size = input_size
@@ -79,7 +65,8 @@ class NeuralNetwork:
         self.iterations = iterations
         self.lmd = lmd
         self.conn_n = get_connections_n(self.hidden_size, self.input_size, self.output_size)
-        self.weights = weights_ if isinstance(weights_, list) and len(weights_) == self.conn_n else np.random.randn(self.conn_n)
+        self.weights = weights_ if isinstance(weights_, list) and len(weights_) == self.conn_n else np.random.randn(
+            self.conn_n)
         self.hidden_neurones = []
 
     def set_iterations(self, iterations):
@@ -107,58 +94,39 @@ class NeuralNetwork:
     def get_out_fS(self, hidden_fS):
         return f(np.dot(hidden_fS, self.weights[self.conn_n - self.hidden_size:]))
 
-    # def get_adjusted_weights(self, delta):
-    #     for neuron in self.hidden_neurones:
-    #         neuron.set_delta(delta * self.lmd * df(neuron))
-
-    # def forward(self, inputs):
-     #     sum = np.dot(self.get_hidden_weights(), inputs)
-
-
     def train(self):
-        # for iteration in 2000:
-        for iteration in range(self.iterations):
+        for iteration in range(1):
             self.initialize_hidden()
             inputs = [2.65, 5.60, 1.21]
             output_weights = self.weights[self.conn_n - self.hidden_size:]
             hidden_fS = self.get_hidden_fS(inputs)
             out_fS = self.get_out_fS(hidden_fS)  #############
-            # error = 5.48 - out_fS  ##########################
-            error = out_fS - 5.48  ##########################
+            error = 5.48 - out_fS  ##########################
             delta = error * df(out_fS)
 
             # get adjusted weights for hidden
             for i in range(len(self.hidden_neurones)):
                 neuron = self.hidden_neurones[i]
-                # print(output_weights[i], "  -->  ", neuron.neuron_fS)
                 neuron.set_delta(delta * output_weights[i] * df(neuron.neuron_fS))
 
             res = []
+            # res = np.array([np.array(i.weights) - i.delta * self.lmd * np.array(inputs) for i in self.hidden_neurones])
             for i in self.hidden_neurones:
-                delta_w = i.delta * self.lmd * np.array(inputs)
-                result_w = np.array(i.weights) - delta_w
-                res = np.concatenate([res, result_w])
+                res = np.concatenate([res, np.array(i.weights) - i.delta * self.lmd * np.array(inputs)])
                 # print(i, '\n', 'Δw', delta_w, '\n', '‾w', result_w, '\n')
 
             # get adjusted weights for output
-            delta_w_out = delta * self.lmd * np.array(hidden_fS)
-            result_w_out = np.array(output_weights) - delta_w_out
-            # print('         Δw', delta_w_out)
-            # print('         ‾w', result_w_out)
-            #
-            # # result = res + result_w_out
-            # print(np.concatenate([res, result_w_out]))
-            self.weights = np.concatenate([res, result_w_out])
+            result_w_out = np.array(output_weights) - delta * self.lmd * np.array(hidden_fS)
+
+
+            print(res)
+            print(result_w_out)
+            # self.weights = np.concatenate([res, result_w_out])
+            # print(self.weights)
 
         self.initialize_hidden()
-        inputs = [2.65, 5.60, 1.21]
-        output_weights = self.weights[self.conn_n - self.hidden_size:]
-        hidden_fS = self.get_hidden_fS(inputs)
-        out_fS = self.get_out_fS(hidden_fS)
-        print(self.hidden_neurones)
+        out_fS = self.get_out_fS(self.get_hidden_fS([2.65, 5.60, 1.21]))
         print(out_fS)
-
-
 
 
 data = [2.65, 5.60, 1.21, 5.48, 0.73, 4.08, 1.88, 5.31, 0.78, 4.36, 1.71, 5.62, 0.43, 4.21, 1.21]
@@ -172,24 +140,11 @@ nn.train()
 # print(nn.get_hidden_weights())
 
 
-
-
-
-
-
 # w = np.array_split([-0.1, 0.2, 0.1, 0.2, 0.5, -0.7, 0.1, -0.3, 0.5, 0.3, 0.6, -0.4], 3)
 # print(w)
 # train_data = np.array([[data[ind], data[ind + 1], data[ind + 2]] for ind in range(10)])
 # print(f(np.dot(f(np.dot(train_data, w)), [0.3, 0.3, 0.2, 0.1])))
 #
-
-
-
-
-
-
-
-
 
 
 # nn.train()
